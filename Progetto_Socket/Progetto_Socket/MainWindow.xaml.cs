6940ab22d64ee694e1e27e753669fe395d4556cb
FrancescoTonelli <francesco.tonelli01@gmail.com>
2022-04-14 17:34:12 +0200

    Completamento livello base (completamento codice causa salvataggio sbagliato)

--- Progetto_Socket/Progetto_Socket/MainWindow.xaml.cs
+++ Progetto_Socket/Progetto_Socket/MainWindow.xaml.cs
@@ -23,23 +23,44 @@ namespace Progetto_Socket
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        Socket calza;
+        //creo l'oggetto socket
+        Socket socket;
+        //creo un timer per la recezione dei messaggi
+        DispatcherTimer dTimer;
         public MainWindow()
         {
             try
             {
                 InitializeComponent();
 
-                calza = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                //inizializzo l'oggetto socket specificando il tipo di indirizzi utilizzato (IP), il tipo di socket (datagram)
+                //e il tipo di protocollo (UDP)
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
+                //creo l'oggetto che ospita l'indirizzo IP locale
                 IPAddress local = IPAddress.Any;
+
+                //creo un endopint associando l'indirizzo locale alla porta da usare per la ricezione
                 IPEndPoint local_endpoint = new IPEndPoint(local.MapToIPv4(), 65000);
 
-                lblRx.Content = "Ricezione messaggi attiva";
+                //associo l'endpoint alla socket
+                socket.Bind(local_endpoint);
 
-                calza.Bind(endpoint);
-            }catch(Exception ex)
+
+                //inizializzo l'oggetto timer
+                dTimer = new DispatcherTimer();
+                //associo un evento al tick del timer
+                dTimer.Tick += new EventHandler(aggiornamento);
+                //imposto un intervallo tra i tick del timer (250 ms)
+                dTimer.Interval += new TimeSpan(0, 0, 0, 0, 250);
+                //avvio il timer
+                dTimer.Start();
+
+
+                //stampo sull'interfaccia che il programma è pronto a ricevere messaggi
+                lblRx.Content = "Ricezione messaggi attiva";
+            }
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
@@ -49,18 +70,64 @@ namespace Progetto_Socket
         {
             try
             {
+                //inizializzo l'indirizzo IP da raggiungere in base a quanto inserito dall'utente
                 IPAddress remote = IPAddress.Parse(txtIP.Text);
 
+                //inizializzo l'endpoint da raggiungere associando l'indirizzo appena creato e la porta da raggiungere specificata
+                //dal mittente
                 IPEndPoint remote_endpoint = new IPEndPoint(remote, int.Parse(txtPorta.Text));
 
+                //ricavo il messaggio da inviare codificando la stringa scritta dall'utente e convertendola
+                //ad un array di byte
                 byte[] mex = Encoding.UTF8.GetBytes(txtMex.Text);
 
-                calza.SendTo(mex, remote_endpoint);
+                //invio l'array messaggio all'endpoint ricevitore, tramite l'oggetto socket
+                socket.SendTo(mex, remote_endpoint);
 
-            }catch(Exception ex)
+                //aggiungo nella listbox anche i messaggi che scrivo io stesso
+                lstBox.Items.Add("Me: " + Encoding.Default.GetString(mex));
+
+            }
+            catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        private void aggiornamento(object sender, EventArgs e)
+        {
+            try
+            {
+                //definisco una variabile int per usarla nell'if come variabile d'appoggio
+                int nBytes;
+                //se ci sono socket in attesa
+                if ((nBytes = socket.Available) > 0)
+                {
+                    //inizializzo un array di byte che userò come buffer
+                    byte[] buffer = new byte[nBytes];
+                    //inizializzo un oggetto EndPoint da usare dopo
+                    EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                    //aggiorno in nBytes il numero di bytes ricevuti dalla socket, il messaggio ricevuto in buffer e l'endpoint
+                    //del mittente
+                    nBytes = socket.ReceiveFrom(buffer, ref remoteEndPoint);
+                    //converto l'indirizzo IP di arrivo in un oggetto string
+                    string from = ((IPEndPoint)remoteEndPoint).Address.ToString();
+                    //decodifico in una stringa il messa
+                    string messaggio = Encoding.UTF8.GetString(buffer, 0, nBytes);
+
+                    //aggiungo nella listbox il messaggio appena ricevuto
+                    lstBox.Items.Add(from + ": " + messaggio);
+                }
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            //pulisco la listbox con la chat
+            lstBox.Items.Clear();
         }
     }
 }
